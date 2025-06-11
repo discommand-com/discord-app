49ab54cb445644a51153be72a48d8bc7c2b1665d
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-06-11 15:19:58 -0400

    Update messageCreate.mjs

--- src/events/messageCreate.mjs
+++ src/events/messageCreate.mjs
@@ -6,46 +6,38 @@ export default async function (message) {
     log.debug('messageCreate', { message });
     try {
         const myId = global.client.user.id;
-        log.debug('myId', { myId });
         if (message.author.id === myId) {
-            log.debug('Message is from myself, skipping');
             return;
         }
         const mentionsMe = message.mentions.users.has(myId);
-        log.debug('mentionsMe', { mentionsMe });
         const isReplyToMe = message.reference && message.channel && myId
             ? (await message.channel.messages.fetch(message.reference.messageId)).author.id === myId
             : false;
-        log.debug('isReplyToMe', { isReplyToMe });
         let mentionsMyRole = false;
         if (message.mentions.roles.size > 0 && message.guild && message.guild.members) {
-            log.debug('Checking for role mentions');
             const me = await message.guild.members.fetch(myId);
-            log.debug('Fetched self member', { me });
             const myRoles = me.roles.cache;
-            log.debug('myRoles', { myRoles: myRoles.map(r => r.id) });
             mentionsMyRole = message.mentions.roles.some(role => myRoles.has(role.id));
-            log.debug('mentionsMyRole', { mentionsMyRole });
         }
         if (!mentionsMe && !isReplyToMe && !mentionsMyRole) {
-            log.debug('Message does not mention me, reply to me, or mention my role. Skipping.');
             return;
         }
-        const messages = await message.channel.messages.fetch({ limit: 100 });
-        log.debug('Fetched channel messages', { count: messages.size });
-        const history = messages.map(m => ({
+        const historyMessages = await message.channel.messages.fetch({ limit: 100 });
+        const history = historyMessages.map(m => ({
             id: m.id,
-            content: m.content,
+            content: m.cleanContent,
             author: m.author?.id,
             timestamp: m.createdTimestamp
         }));
-        log.debug('history', { historyCount: history.length });
+        const authorRoles = (message.member && message.member.roles)
+            ? message.member.roles.cache.map(role => role.id)
+            : [];
         await publish('inbox', {
             rsvp: `discord_${myId}`,
+            authorRoles,
             message,
             history,
         });
-        log.debug('Published to inbox');
     } catch (err) {
         log.error('Failed to publish message to inbox queue', err);
     }
