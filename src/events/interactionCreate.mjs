9b3d87ae4344f75512433b0083a52f848f05ab25
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-06-11 13:44:43 -0400

    Update interactionCreate.mjs

--- src/events/interactionCreate.mjs
+++ src/events/interactionCreate.mjs
@@ -2,9 +2,9 @@ import log from '../log.mjs';
 
 // Event handler for interactionCreate
 export default async function (interaction) {
-    log.debug('createInteraction', interaction);
-    const handler = global.commands[interaction.commandName];
-    if (handler) {
-        await handler(interaction);
-    }
+    log.debug('createInteraction', { interaction });
+    interaction.reply({
+        content: 'Not Implemented',
+        flags: 1 << 6 // EPHEMERAL 
+    });
 }
