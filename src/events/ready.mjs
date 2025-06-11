ee50af3288c2c843c9b8b6ea4e985cd4bdefe5e4
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-06-11 15:41:11 -0400

    Update ready.mjs

--- src/events/ready.mjs
+++ src/events/ready.mjs
@@ -1,12 +1,12 @@
 import log from '../log.mjs';
-import { getAppPresence } from '../db.mjs';
+import { getAppTitle } from '../db.mjs';
 
 // Event handler for ready
 export default async function (client) {
     log.info(`Logged in as ${client.user.tag}`);
-    const dbPresence = await getAppPresence(client.user.id);
+    const title = await getAppTitle(client.user.id);
     client.user.setPresence({
-        activities: dbPresence ? [{ name: dbPresence, type: 4 }] : [],
+        activities: title ? [{ name: title, type: 4 }] : [],
         status: 'online'
     });
 }
